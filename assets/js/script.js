02d212bfd8d180def23678c08b6494c415876520
joshgit <jschermann91@gmail.com>
2021-11-03 08:20:47 -0500

    saving and loading working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
-// Hello, Earthlings!
+// Date at top of screen
+let today = moment().format("dddd, MMMM Do");
+let currentHour = moment().hour();
+alert(currentHour);
 
-/// Moment.js, bootstrap, and jQuery have been added
+// load tasks
+loadTasks = function () {
+  $("#nineAm").val(localStorage.getItem("9AM"));
+  $("#tenAm").val(localStorage.getItem("10AM"));
+};
 
-const today = moment();
+// function to render colors
 
-$("#currentDay").text(today);
-$("#currentDay").addClass("text-center");
+// Wait for document to render before loading times
+$(document).ready(function () {
+  // Date at top of screen
+  $("#currentDay").text(today);
+  $("#currentDay").addClass("text-center");
+  // load tasks already saved
+  loadTasks();
+  // invoke render colors function here,
+});
+
+// Save task
+$(".saveBtn").on("click", function () {
+  // Siblings function https://www.geeksforgeeks.org/jquery-siblings-with-examples/
+  let hour = $(this).siblings(".hour").text();
+  let task = $(this).siblings(".description").val();
+
+  // Add pair to local storage
+  localStorage.setItem(hour, task);
+});
+
+///variables
+//functions
+//event listeners
