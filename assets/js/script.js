c854a2fa9af05f214e6ceceed2bdc761e7f20af9
joshgit <jschermann91@gmail.com>
2021-11-03 09:16:55 -0500

    color am

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,21 @@ loadTasks = function () {
 };
 
 // function to render colors
+let renderColor = function () {
+  $(".time-block").each(function () {
+    let timeBlock = parseInt($(this).text().split("am")[0]);
+
+    if (timeBlock == currentHour) {
+      $(this).addClass("present");
+    }
+    if (timeBlock > currentHour) {
+      $(this).addClass("future");
+    }
+    if (timeBlock < currentHour) {
+      $(this).addClass("past");
+    }
+  });
+};
 
 // Wait for document to render before loading times
 $(document).ready(function () {
@@ -19,6 +34,7 @@ $(document).ready(function () {
   // load tasks already saved
   loadTasks();
   // invoke render colors function here,
+  renderColor();
 });
 
 // Save task
